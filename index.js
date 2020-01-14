2d2552281a52efbdbb72beb1db9b22411774fce0
Ryan Lemos <ryanlucas.lemos@gmail.com>
2020-01-14 18:06:58 -0300

    cria o app express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Hello'
+  });
+});
+
+app.listen(3333);
